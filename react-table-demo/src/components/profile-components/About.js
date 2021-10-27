5e3b7848db23cbda45d47a6eb07fbe7397700980
Dennis Hahn <dehahn@cs.stonybrook.edu>
2021-10-27 18:03:19 -0400

    add layouts for posts and about section of profile

---
+++ react-table-demo/src/components/profile-components/About.js
@@ -0,0 +1,25 @@
+
+function About(props) {
+
+    return (
+        <div>
+            <p>This is were the user will enter information about themselves.</p>
+            <ul>
+                <li>
+                    <a href="https://github.com/">Github</a>
+                </li>
+                <li>
+                    <a href="https://facebook.com/">Facebook</a>
+                </li>
+                <li>
+                    <a href="https://twitter.com/">Twitter</a>
+                </li>
+                <li>
+                    <a href="https://stonybrook.edu/">Stony Brook</a>
+                </li>
+            </ul>
+        </div>
+    );
+}
+
+export default About;
---
+++ react-table-demo/src/components/profile-components/Quizzes.js
@@ -0,0 +1,8 @@
+function Quizzes(props) {
+
+    return (
+        <p>Quizzes</p>
+    );
+  }
+  
+  export default Quizzes;
