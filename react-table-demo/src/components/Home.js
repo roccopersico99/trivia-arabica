b57e656a378a774632f533fbfdf456ee5500bfbc
NPradlik <56689911+NPradlik@users.noreply.github.com>
2021-10-27 15:10:33 -0400

    add/read user data prototype
    added firestore.js for containing backend functions
    added create user function
    added get user by ID function
    added test buttons in Home.js to showcase these functionalities

--- react-table-demo/src/components/Home.js
+++ react-table-demo/src/components/Home.js
@@ -1,12 +1,25 @@
 import '../App.css';
 import Background from './Background.js'
+import * as FirestoreBackend from '../services/firestore.js'
 
 
 function Home() {
-
   return (
     <Background>
       <p>Home</p>
+      <button onClick={()=>{
+        const usr_query = FirestoreBackend.getUser('1');
+        console.log(usr_query);
+        usr_query.then((query_snapshot)=>{
+          query_snapshot.forEach((user) => {
+            console.log(user.data())
+            console.log(user.data().display_name)
+            console.log(user.data().user_bio)
+          });
+        });
+        }}>Test get user where ID=1</button>
+      <button onClick={()=>FirestoreBackend.createUser("gus", '2')}>
+        Test add user gus ID:2</button>
     </Background>
   );
 }
---
+++ react-table-demo/src/services/firestore.js
@@ -0,0 +1,38 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
+import { collection, query, where, getDocs, limit } from "firebase/firestore";
+
+
+
+import "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBv4itSAtnCyRSbobSEsuk3LnOBs6V0-8g",
+    authDomain: "mystical-factor-327614.firebaseapp.com",
+    projectId: "mystical-factor-327614",
+    storageBucket: "mystical-factor-327614.appspot.com",
+    messagingSenderId: "232679417776",
+    appId: "1:232679417776:web:7413832550f5b9782d5875"
+  };
+  
+const app = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+const userRef = collection(db, 'users')
+
+export const createUser = (userName, userId) => {
+    return db.collection('users')
+        .add({
+            display_name: userName,
+            user_id: userId,
+            medals: 0,
+            profile_image: "",
+            user_bio: "",
+            featured_quiz: ""
+        });
+};
+
+//this returns a promise object 
+export const getUser = (userId, observer) => {
+    const q = query(userRef, where("user_id", "==", userId), limit(1));
+    return getDocs(q)
+};
