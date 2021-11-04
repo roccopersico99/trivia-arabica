d7046d0bf1b69f73966115b33f76dd633c160de6
dennis12hahn <dennis12hahn@gmail.com>
2021-11-04 10:01:52 -0400

    fix name and button on profile page

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,10 +1,10 @@
 import "../App.css";
 import Background from "./Background.js";
-import { useAuthState } from '../Context/index'
-import 'bootstrap/dist/css/bootstrap.css';
-import * as FirestoreBackend from '../services/Firestore.js'
-import { useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { useAuthState } from "../Context/index";
+import "bootstrap/dist/css/bootstrap.css";
+import * as FirestoreBackend from "../services/Firestore.js";
+import { useState } from "react";
+import { useParams } from "react-router-dom";
 
 import {
   Image,
@@ -27,31 +27,28 @@ function Profile() {
 
   const [about, setAbout] = useState("");
   const [name, setName] = useState("");
-  const [profileImage, setProfileImage] = useState("")
+  const [profileImage, setProfileImage] = useState("");
 
-  const [refreshKey, setRefreshKey] = useState(0)
-
+  const [refreshKey, setRefreshKey] = useState(0);
 
   const params = useParams();
   let currentUser = "";
-
-
 
   const getAbout = async () => {
     const usr_query = FirestoreBackend.getUser(currentUser);
     usr_query.then((query_snapshot) => {
       query_snapshot.forEach((user) => {
-        setAbout(user.data().user_bio)
-        setName(user.data().display_name)
-        setProfileImage(user.data().profile_image)
+        setAbout(user.data().user_bio);
+        setName(user.data().display_name);
+        setProfileImage(user.data().profile_image);
       });
     });
-  }
+  };
 
   const setupProfile = async () => {
-    currentUser = params.id
-    getAbout()
-  }
+    currentUser = params.id;
+    getAbout();
+  };
 
   const setAboutText = (val) => {
     const usr_query = FirestoreBackend.getUser(userDetails.id);
@@ -63,16 +60,16 @@ function Profile() {
         FirestoreBackend.updateData(userRef, data);
       });
     });
-    setRefreshKey(refreshKey + 1)
-  }
+    setRefreshKey(refreshKey + 1);
+  };
 
-
-
-  let quizzes = [{
+  let quizzes = [
+    {
       id: "1",
       title: "Baseball Quiz!!!",
       description: "This quiz is about MLB history.",
-      image: "https://upload.wikimedia.org/wikipedia/commons/4/40/Heyward_lines_into_double_play_%2828356212176%29.jpg",
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/4/40/Heyward_lines_into_double_play_%2828356212176%29.jpg",
       creator: "1",
       platform: "1",
       questions: [{ title: "First Question", choices: [1, 2, 3, 4] }],
@@ -82,7 +79,8 @@ function Profile() {
       id: "2",
       title: "Football Quiz!!!",
       description: "This quiz is about NFL history.",
-      image: "https://wallpaperbat.com/img/157590-troy-polamalus-hall-of-fame-career-was-pure-chaos.jpg",
+      image:
+        "https://wallpaperbat.com/img/157590-troy-polamalus-hall-of-fame-career-was-pure-chaos.jpg",
       creator: "1",
       platform: "1",
       questions: [{ title: "First Question", choices: [1, 2, 3, 4] }],
@@ -92,14 +90,16 @@ function Profile() {
       id: "3",
       title: "Basketball Quiz!!!",
       description: "This quiz is about NBA history.",
-      image: "https://theundefeated.com/wp-content/uploads/2018/03/gettyimages-587625016.jpg",
+      image:
+        "https://theundefeated.com/wp-content/uploads/2018/03/gettyimages-587625016.jpg",
       creator: "1",
       platform: "1",
       questions: [{ title: "First Question", choices: [1, 2, 3, 4] }],
       ratings: [],
     },
   ];
-  let posts = [{
+  let posts = [
+    {
       title: "First Post",
       content: "This is my first post",
       date: "10/28/2021",
@@ -113,8 +113,12 @@ function Profile() {
   let user = {
     id: "1",
     display_name: name,
-    profile_picture: profileImage === "" ? "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" : profileImage,
-    banner_image: "https://image.freepik.com/free-vector/abstract-dotted-banner-background_1035-18160.jpg",
+    profile_picture:
+      profileImage === ""
+        ? "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
+        : profileImage,
+    banner_image:
+      "https://image.freepik.com/free-vector/abstract-dotted-banner-background_1035-18160.jpg",
     about: {
       content: "About Me",
       description: about,
@@ -132,44 +136,47 @@ function Profile() {
   };
 
   if (name === "") {
-    setupProfile()
+    setupProfile();
     return (
       <Background>
-        <Spinner style={{marginTop:"100px"}} animation="border" role="status">
+        <Spinner
+          style={{ marginTop: "100px" }}
+          animation="border"
+          role="status"
+        >
           <span className="visually-hidden">Loading...</span>
         </Spinner>
       </Background>
-    )
+    );
   }
   return (
     <Background>
       <Image
-        style={{ width: "100%", height: "200px", "objectFit": "cover" }}
+        style={{ width: "100%", height: "200px", objectFit: "cover" }}
         src={user.banner_image}
         alt="Banner"
       ></Image>
 
       <Container>
         <Row>
-          <Col lg="3">
+          <Col lg="3" style={{ position: "relative", top: "-25px" }}>
             <Image
               style={{
-                top: "-25px",
-                position: "relative",
                 height: "150px",
                 width: "150px",
               }}
               src={user.profile_picture}
               alt="Profile"
             ></Image>
+            <Stack>
+              <div>{user.display_name}</div>
+              <Button
+                style={{ width: "100px", margin: "auto", position: "relative" }}
+              >
+                Follow
+              </Button>
+            </Stack>
           </Col>
-
-          <Stack>
-            <br></br>
-            <div>{user.display_name}</div>
-            <br></br>
-            <Button>Follow</Button>
-          </Stack>
         </Row>
 
         <Row>
