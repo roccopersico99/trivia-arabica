93e767ea8f74226c27aaee90796cc64db3ef7220
Dennis Hahn <dehahn@cs.stonybrook.edu>
2021-11-05 10:27:43 -0400

    fix quizcard image formatting

--- src/components/profile-components/QuizCard.js
+++ src/components/profile-components/QuizCard.js
@@ -3,12 +3,15 @@ import { Link } from "react-router-dom";
 import React from "react";
 
 function QuizCard(props) {
-
   return (
     <Card as={Col} style={{ margin: "10px" }}>
       <Card.Body>
         <Card.Title>{props.quiz?.title}</Card.Title>
-        <Card.Img variant="top" src={props.quiz?.image}></Card.Img>
+        <Card.Img
+          style={{ width: "100px", "background-size": "cover" }}
+          variant="top"
+          src={props.quiz?.image}
+        ></Card.Img>
         <Card.Text>{props.quiz?.description}</Card.Text>
         <Link
           to={{ pathname: "/preview/" + props.quiz?.id, state: props.quiz }}
