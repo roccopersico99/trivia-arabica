aca5e7771d390b747ee1188ec2e2ba43a5e47c55
BuildTools <rocco.persico@stonybrook.edu>
2021-11-05 12:15:24 -0400

    cleaned up discover page

--- src/components/Discover.js
+++ src/components/Discover.js
@@ -1,33 +1,56 @@
-import { Container, Row } from "react-bootstrap";
+import { Container, Row, Stack, InputGroup, FormControl, Button, Dropdown, DropdownButton } from "react-bootstrap";
 import QuizCard from "./profile-components/QuizCard";
-import { useEffect } from 'react'
+import { useState } from 'react'
 import Background from "./Background";
 
 function Discover() {
 
-//   useEffect(() => {
-//     console.log(quizzes);
-//   }, [quizzes])
+    const [completedFilter, setCompletedFilter] = useState("Completed");
+    const [searchFilter, setSearchFilter] = useState("SmartSort");
 
-//   return (
-//     <Container>
-//       {/*
-//         get number of quizzes from this user
-//         generate n rows for every i (3) quizzes
-//         */}
-//         Welcome to Quiz Discover!
+    function handleSearch(target) {
+        console.log("searching for: '", target.textContent, "'")
+    }
 
-//       <Row>
-//         {quizzes.map((quiz, index) => {
-//           return (  <QuizCard quiz={quiz} key={index}></QuizCard>)
-//         })}
-//       </Row>
-//     </Container>
-    
-//   );
     return (
         <Background>
+            <br></br>
             <h1>Welcome to the Quiz Discover Page!</h1>
+            <br></br>
+            <Stack direction="horizontal" gap={2} style={{margin:"10px"}}>
+                <InputGroup>
+                    <Button onClick={(e) => handleSearch(e.target)} variant="dark" id="button-addon1">🔍</Button>
+                    <FormControl aria-label="Example text with button addon" placeholder="Enter search terms..." aria-describedby="basic-addon1"/>
+                </InputGroup>
+                <DropdownButton variant="outline-secondary" title={completedFilter + " "} id="input-group-dropdown-1">
+                    <Dropdown.Item as="button"><div onClick={(e) => setCompletedFilter(e.target.textContent)}>Completed</div></Dropdown.Item>
+                    <Dropdown.Item as="button"><div onClick={(e) => setCompletedFilter(e.target.textContent)}>Not Completed</div></Dropdown.Item>
+                    <Dropdown.Item as="button"><div onClick={(e) => setCompletedFilter(e.target.textContent)}>All Quizzes</div></Dropdown.Item>
+                </DropdownButton>
+                <DropdownButton variant="outline-secondary" title={searchFilter + " "} id="input-group-dropdown-2">
+                    <Dropdown.Item as="button"><div onClick={(e) => setSearchFilter(e.target.textContent)}>Ascending</div></Dropdown.Item>
+                    <Dropdown.Item as="button"><div onClick={(e) => setSearchFilter(e.target.textContent)}>Descending</div></Dropdown.Item>
+                    <Dropdown.Item as="button"><div onClick={(e) => setSearchFilter(e.target.textContent)}>SmartSort</div></Dropdown.Item>
+                </DropdownButton>
+            </Stack>
+            <br></br>
+            <Container>
+                <Row>
+                    <QuizCard></QuizCard>
+                    <QuizCard></QuizCard>
+                    <QuizCard></QuizCard>
+                </Row>
+                <Row>
+                    <QuizCard></QuizCard>
+                    <QuizCard></QuizCard>
+                    <QuizCard></QuizCard>
+                </Row>
+                <Row>
+                    <QuizCard></QuizCard>
+                    <QuizCard></QuizCard>
+                    <QuizCard></QuizCard>
+                </Row>
+            </Container>
         </Background>
     );
 }
