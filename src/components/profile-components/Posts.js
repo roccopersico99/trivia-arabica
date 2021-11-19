76cb58fee65b46f64ca72859f665e037d9a595bd
Dennis Hahn <dennis12hahn@gmail.com>
2021-11-19 11:36:46 -0500

    add button to add post

--- src/components/profile-components/Posts.js
+++ src/components/profile-components/Posts.js
@@ -3,17 +3,19 @@ import { Container, ListGroup, Card, Button } from "react-bootstrap";
 function Posts(props) {
   return (
     <Container>
+      <Button className="my-3">Add Post</Button>
       <ListGroup>
-        {props.posts?.map((post)=>(
-          <Card>
-            <Card.Body>
-              <Card.Title>{post.post_title + " by userID: " + post.post_creator}</Card.Title>
-              <Card.Text>{post.publish_date.toDate().toString() + "\n" + post.post_text}</Card.Text>
-              <Button variant="success">Like</Button>
-              <Button variant="danger">Dislike</Button>
-            </Card.Body>
-          </Card>
-        ))}
+        {props.posts?.length > 0 ?
+          (props.posts?.map((post) => (
+            <Card>
+              <Card.Body>
+                <Card.Title>{post.post_title + " by userID: " + post.post_creator}</Card.Title>
+                <Card.Text>{post.publish_date.toDate().toString() + "\n" + post.post_text}</Card.Text>
+                <Button variant="success">Like</Button>
+                <Button variant="danger">Dislike</Button>
+              </Card.Body>
+            </Card>
+          ))) : <div>Make your first post!</div>}
       </ListGroup>
     </Container>
   );
