0bd484f49549ff65c22564b00cb46e5b2ed1d6b6
Dennis Hahn <dennis12hahn@gmail.com>
2021-12-02 16:59:16 -0500

    load profile image when logged in

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,18 +1,25 @@
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { default as logo } from "../logo.svg";
-import { default as userIcon } from "../person-circle.svg";
+import { default as defaultUserIcon } from "../person-circle.svg";
 import { default as settingsIcon } from "../settings-icon.png";
 import LoginLogout from "./LoginLogout";
 
 import { useAuthState } from "../Context/index";
 import { useHistory } from "react-router-dom";
-import { useState } from "react"
+import { useState } from "react";
 import SettingsPopup from "./SettingsPopup";
 import * as FirestoreBackend from "../services/Firestore.js";
 
-
 function Navigation() {
   const userDetails = useAuthState();
+  const loggedIn = userDetails.id !== "";
+
+  let userIcon = defaultUserIcon;
+  let userIconStyle = { filter: "invert(1)" };
+  if (loggedIn) {
+    userIcon = userDetails.imageUrl;
+    userIconStyle = {};
+  }
 
   const [modalShow, setModalShow] = useState(false);
 
@@ -20,33 +27,38 @@ function Navigation() {
   const handleOnClick = (event) => {
     history.push({
       pathname: "/profile/" + userDetails.id,
-      state: userDetails
+      state: userDetails,
     });
   };
 
   function saveSettings() {
     // TODO
-    console.log("saving settings...")
+    console.log("saving settings...");
   }
 
   function resetSettings() {
     // TODO
-    console.log("resetting all settings...")
+    console.log("resetting all settings...");
   }
 
   // TODO: Firebase backend gets random quiz ID, but not in time for Nav component to use
   const handleRandom = async () => {
     const random_quiz = await FirestoreBackend.getRandomQuiz();
-    console.log("random quiz id: ", random_quiz)
+    console.log("random quiz id: ", random_quiz);
     history.push({
       pathname: "/preview/" + random_quiz,
-      state: userDetails
+      state: userDetails,
     });
   };
 
   return (
     <div>
-      <SettingsPopup show={modalShow} onHide={() => setModalShow(false)} handleSaveSettings={() => saveSettings()} handleResetSettings={() => resetSettings()} ></SettingsPopup>
+      <SettingsPopup
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+        handleSaveSettings={() => saveSettings()}
+        handleResetSettings={() => resetSettings()}
+      ></SettingsPopup>
       <Navbar collapseOnSelect expand="sm" bg="dark" variant="dark">
         <Container>
           <Navbar.Brand href="/">
@@ -64,9 +76,9 @@ function Navigation() {
               >
                 Profile
               </Nav.Link>
-              <Nav.Link href={
-                  userDetails.user === "" ? "/" : "/creator/"
-                }>Creator</Nav.Link>
+              <Nav.Link href={userDetails.user === "" ? "/" : "/creator/"}>
+                Creator
+              </Nav.Link>
               <Nav.Link href="/discover">Discover</Nav.Link>
               <Nav.Link href="/platforms">Platforms</Nav.Link>
               <Nav.Link onClick={handleRandom}>Random</Nav.Link>
@@ -83,11 +95,16 @@ function Navigation() {
                   width="50"
                   height="50"
                   alt="user icon"
-                  style={{ filter: "invert(1)" }}
+                  style={ userIconStyle }
                 />
               </Navbar.Brand>
               <Navbar.Brand onClick={() => setModalShow(true)}>
-                <img src={settingsIcon} width="50" height="50" alt="settings icon"></img>
+                <img
+                  src={settingsIcon}
+                  width="50"
+                  height="50"
+                  alt="settings icon"
+                ></img>
               </Navbar.Brand>
             </Nav>
           </Navbar.Collapse>
