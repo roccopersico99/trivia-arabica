ca2e0cbc31ca26a6179abf6928c2f92d32a11beb
Dylan Downard <dyldownard@outlook.com>
2021-12-13 12:19:14 -0500

    fixed when user is in no platforms

--- src/components/profile-components/ProfilePlatforms.js
+++ src/components/profile-components/ProfilePlatforms.js
@@ -2,13 +2,21 @@ import { Container } from "react-bootstrap";
 import PlatformCard from "./PlatformCard.js";
 
 function ProfilePlatforms(props) {
-  return (
-    <Container style={{display: "flex", flexWrap:"wrap"}}>
-      {props.platforms.map((platform, index) => (
-          <PlatformCard setFeaturedPlatformProp={props.setFeaturedPlatformProp}  featuredPlatform={props.featuredPlatform} userDetails={props.userDetails} ownProfile={props.ownProfile} platform={platform} key={index}></PlatformCard>
-      ))}
-    </Container>
-  );
+  if (props.platforms.length === 0) {
+    return (
+      <Container>
+        <h3 style={{marginTop:"20px"}}> User is in no platforms </h3>
+      </Container>
+    )
+  } else {
+    return (
+      <Container style={{display: "flex", flexWrap:"wrap"}}>
+        {props.platforms.map((platform, index) => (
+            <PlatformCard setFeaturedPlatformProp={props.setFeaturedPlatformProp}  featuredPlatform={props.featuredPlatform} userDetails={props.userDetails} ownProfile={props.ownProfile} platform={platform} key={index}></PlatformCard>
+        ))}
+      </Container>
+    );
+  }
 }
 
 export default ProfilePlatforms;
